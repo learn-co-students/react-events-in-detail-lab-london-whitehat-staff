7742945591ab7cb95b85e822237e387abb7a6dc5
Alexander Startup <alexander.startup@sky.uk>
2019-03-26 14:57:47 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    coordinates = (event) => {
+        let x = event.clientX;
+        let y = event.clientY;
+        let coords = [x, y]
+        return this.props.onReceiveCoordinates(coords)
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.coordinates}>coordinates</button>
+            </div>
+        )
+    }
+}
